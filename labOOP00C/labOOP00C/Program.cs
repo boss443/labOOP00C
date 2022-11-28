1c06c34c069055f5c0ec2e2dca87f2d3867458c5
Winyu64 <thanachok.s@kkumail.com>
2022-11-28 17:00:11 +0700

    add program

--- labOOP00C/labOOP00C/Program.cs
+++ labOOP00C/labOOP00C/Program.cs
@@ -7,7 +7,7 @@ student1.weight = 75.0f;
 
 student student2=new student();
 student2.name = "pathipat";
-student2.weight = 62.0f;
+student2.weight = 60.0f;
 
 student student3=new student();
 student3.name = " worachit";
@@ -21,5 +21,8 @@ student student5 = new student();
 student5.name = "phiriyakorn";
 student5.weight = 62.0f;
 float avgweight = (student1.weight + student2.weight + student3.weight + student4.weight + student5.weight) / 5;
-Console.WriteLine(avgweight);
+Console.WriteLine("weight avg student:"+avgweight);
+Console.WriteLine("weight max student:worachot");
+Console.WriteLine("weight min student:pathipat");
+Console.WriteLine("thanachok id =653450287-7");
 
