8c8f3aefa86a20963b35cd0df45c4c2009450f72
Winyu64 <thanachok.s@kkumail.com>
2022-11-28 16:54:15 +0700

    add program

---
+++ labOOP00C/labOOP00C/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using labOOP00C;
+
+student student1=new student();
+student1.name = "thanachok";
+student1.weight = 75.0f;
+
+student student2=new student();
+student2.name = "pathipat";
+student2.weight = 62.0f;
+
+student student3=new student();
+student3.name = " worachit";
+student3.weight = 82.5f;
+
+student student4 = new student();
+student4.name = "worachot";
+student4.weight = 90.6f;
+
+student student5 = new student();
+student5.name = "phiriyakorn";
+student5.weight = 62.0f;
+float avgweight = (student1.weight + student2.weight + student3.weight + student4.weight + student5.weight) / 5;
+Console.WriteLine(avgweight);
+
